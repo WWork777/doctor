f813a4f91efef3b0e92654016bd3e2dc21b62195
unknown <parsikovevgenij470@gmail.com>
2025-10-24 00:14:50 +0700

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,48 +1,59 @@
-import './globals.scss';
-import { Wix_Madefor_Display, Inter, TikTok_Sans } from 'next/font/google';
-import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Header/Header';
-
+import "./globals.scss";
+import { Wix_Madefor_Display, Inter, TikTok_Sans } from "next/font/google";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 
 const wixMadefor = Wix_Madefor_Display({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['400','500','600'], // Regular + SemiBold
-  variable: '--font-wix',
-  display: 'swap',
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500", "600"], // Regular + SemiBold
+  variable: "--font-wix",
+  display: "swap",
 });
 
 const tikTokSans = TikTok_Sans({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['400', '500', "600","700","900"],
-  variable: '--font-tikTokSans',
-  display: 'swap',
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500", "600", "700", "900"],
+  variable: "--font-tikTokSans",
+  display: "swap",
 });
 
 const inter = Inter({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['400'],
-  variable: '--font-inter',
-  display: 'swap',
+  subsets: ["latin", "cyrillic"],
+  weight: ["400"],
+  variable: "--font-inter",
+  display: "swap",
 });
 
 export const metadata = {
-  title: 'Кирокосьян Марселла Артуровна',
-  description: 'Врач нейрохирург с огромным стажем работы',
+  icons: {
+    icon: [
+      { rel: "icon", type: "image/svg+xml", url: "/favicon/favicon.svg" },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        url: "/favicon/favicon-96x96.png",
+      },
+    ],
+    shortcut: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html
-      lang='ru'
+      lang="ru"
       className={`${wixMadefor.variable} ${tikTokSans.variable} ${inter.variable}`}
     >
       <body>
-        <Header/>
+        <Header />
         {/* <Header/> */}
-        <main className='main'>
+        <main className="main">
           <div>{children}</div>
         </main>
-            <Footer />
+        <Footer />
       </body>
     </html>
   );
